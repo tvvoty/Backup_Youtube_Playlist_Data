67c48735fb64170f0634d459694f1829d23ec22d
tvvoty <neketa777@gmail.com>
2021-10-06 07:36:47 +0300

    Completed refactoring

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from pytube import YouTube
+# from pytube import YouTube
 from pytube import Playlist
-import re
 import requests
+
+
+import re
 
 
 class VideoObject:
     """Object for storing all videos in the playlist in a list"""
+
+    new_line_character = "\n"  # f-strings don't support back slashes so, yeah...
 
     def __init__(self, thumbnail_path, title, url, author, description):
         self.thumbnail_path = thumbnail_path
@@ -14,50 +18,95 @@ class VideoObject:
         self.author = author
         self.description = description
 
-
-# the_playlist = Playlist('https://www.youtube.com/playlist?list=PL-nKcT3XDGcIuGnq4l92ZRLY2s7rUJENV')
-the_playlist = Playlist('https://www.youtube.com/playlist?list=PL-nKcT3XDGcJT1v19mezBVRvyvDAx8oas')
-video_obj_list = []
-list_count = 0
-for video in the_playlist.videos:
-    video_obj_list.append(VideoObject(
-        f"thumbnails/{video.title}.jpg", video.title, the_playlist.video_urls[list_count], video.author, video.description))
-    img_data = requests.get(video.thumbnail_url).content
-    with open(f'thumbnails/{video.title}.jpg', 'wb') as handler:
-        handler.write(img_data)
-    list_count += 1
-    if list_count == 3:
-        break
+    def entry_html(vid_obj):
+        html = ((f'<div class="thumbnail"><img src="{vid_obj.thumbnail_path}"></div><div class="title">'
+                 f'<div class="title">{vid_obj.title}<br><br></div>'
+                 f'<div class="url">{vid_obj.url}<br><br></div>'
+                 f'<div class="author"><b>{vid_obj.author}</b><br><br></div>'
+                 f'<div class="description">{re.sub(VideoObject.new_line_character, r"<br>", vid_obj.description)}<br><br></div>'))
+        return html
 
 
-html_visual_list = "html_visual_list.html"
-try:
-    new_line_character = "\n"
+def init_promt():
+    while True:
+        mode = input(('Enter "add" into the console to add a new playlist\n'
+                      'Enter "check" to check a playlist:\n'))
+        if mode == "add" or mode == "check":
+            return mode
+        print("Invalid input, lets try again.\n")
+
+
+def get_playlist_url_from_user():
+    playlist_url = input('Enter the playlist url')
+    return playlist_url
+
+
+def get_playlist_name_from_user():
+    playlist_url = input(
+        'Enter the name for the folder the data for the playlist will be stored in:\n')
+    return playlist_url
+
+
+def create_video_obj_list(playlist_dir_name):
+    # the_playlist = Playlist('https://www.youtube.com/playlist?list=PL-nKcT3XDGcIuGnq4l92ZRLY2s7rUJENV')
+    # the_playlist = Playlist('https://www.youtube.com/playlist?list=PL-nKcT3XDGcJT1v19mezBVRvyvDAx8oas')
+    the_playlist = get_playlist_url_from_user()
+    video_obj_list = []
+    list_count = 0
+    for video in the_playlist.videos:
+        video_obj_list.append(VideoObject(
+            f"{playlist_dir_name}/thumbnails/{video.title}.jpg", video.title, the_playlist.video_urls[list_count], video.author, video.description))
+        img_data = requests.get(video.thumbnail_url).content
+        with open(f'{playlist_dir_name}/thumbnails/{video.title}.jpg', 'wb') as handler:
+            handler.write(img_data)
+        list_count += 1
+        if list_count == 3:
+            break
+    return video_obj_list
+
+
+def get_tamplate_html():
     with open("html_template.html", mode='r', encoding='utf-8') as template:
         for line in template:
             html_from_template = template.readlines()
             #print(f'html lines are: {html_lines}')
-    with open(html_visual_list, mode='w', encoding='utf-8') as f:
-        for line in html_from_template[0:70]:
-            # print(line)
-            f.write(line)
-        for vid_obj in video_obj_list:
-            f.write((f'<div class="thumbnail"><img src="{vid_obj.thumbnail_path}"></div><div class="title">'
-                     f'<div class="title">{vid_obj.title}<br><br></div>'
-                     f'<div class="url">{vid_obj.url}<br><br></div>'
-                     f'<div class="author"><b>{vid_obj.author}</b><br><br></div>'
-                     f'<div class="description">{re.sub(new_line_character, r"<br>", vid_obj.description)}<br><br></div>'))
-        for line in html_from_template[70:74]:
-            f.write(line)
-except Exception as e:
-    print(f"Something wrong with your file or name: \n{e}\n Lets try again")
+        return html_from_template
 
-with open("videoslist.txt", mode='w', encoding='utf-8') as f:
-    obj_count = 0
-    f.write('ext_vid_obj_list = [')
-    for obj in video_obj_list:
-        f.write(
-            f'obj{obj_count}({obj.thumbnail_path}, {obj.title}, {obj.url}, {obj.author}, {obj.description}),\n\n')
-        obj_count += 1
 
-# yt = YouTube('https://www.youtube.com/watch?v=qv3DQNchmHg&list=PL-nKcT3XDGcJT1v19mezBVRvyvDAx8oas')
+def write_to_visual_list_html_file(playlist_dir_name):
+    html_from_template = get_tamplate_html()
+    html_visual_list = "{playlist_dir_name}/html_visual_list.html"
+    try:
+        with open(html_visual_list, mode='w', encoding='utf-8') as f:
+            for line in html_from_template[0:70]:
+                # print(line)
+                f.write(line)
+            for vid_obj in video_obj_list:
+                f.write(VideoObject.entry_html(vid_obj))
+            for line in html_from_template[70:74]:
+                f.write(line)
+    except Exception as e:
+        print(f"Something wrong with your file or name: \n{e}\n Lets try again")
+
+
+def write_vid_obj_list(video_obj_list, playlist_dir_name):
+    with open("videoslist.txt", mode='w', encoding='utf-8') as f:
+        obj_count = 0
+        f.write('ext_vid_obj_list = [')
+        for obj in video_obj_list:
+            f.write(
+                f'obj{obj_count}({obj.thumbnail_path}, {obj.title}, {obj.url}, {obj.author}, {obj.description}),\n\n')
+            obj_count += 1
+
+
+def create_playlist_data_backup():
+    playlist_dir_name = get_playlist_name_from_user()
+    vid_obj_list = create_video_obj_list(playlist_dir_name)
+    write_to_visual_list_html_file(playlist_dir_name)
+    write_vid_obj_list(vid_obj_list, playlist_dir_name)
+
+
+# the_playlist = Playlist('https://www.youtube.com/playlist?list=PL-nKcT3XDGcIuGnq4l92ZRLY2s7rUJENV')
+# the_playlist = Playlist('https://www.youtube.com/playlist?list=PL-nKcT3XDGcJT1v19mezBVRvyvDAx8oas')
+if init_promt() == "add":
+    create_playlist_data_backup()
--- try.py
+++ try.py
@@ -1,8 +1,7 @@
-from pytube import YouTube
+# from pytube import YouTube
 from pytube import Playlist
-# yt = YouTube('https://www.youtube.com/watch?v=qv3DQNchmHg&list=PL-nKcT3XDGcJT1v19mezBVRvyvDAx8oas')
-
-# print(yt.description)
+import re
+import requests
 
 
 def init_promt():
@@ -19,8 +18,10 @@ def get_playlist_url_from_user():
     return playlist_url
 
 
-def create_playlist_data_backup:
-    pass
+def get_playlist_name_from_user():
+    playlist_url = input(
+        'Enter the name for the folder the data for the playlist will be stored in:\n')
+    return playlist_url
 
 
 def create_video_obj_list():
@@ -33,9 +34,46 @@ def create_video_obj_list():
         video_obj_list.append(VideoObject(
             f"thumbnails/{video.title}.jpg", video.title, the_playlist.video_urls[list_count], video.author, video.description))
         img_data = requests.get(video.thumbnail_url).content
-        with open(f'thumbnails/{video.title}.jpg', 'wb') as handler:
+        with open(f'{get_playlist_name_from_user}/thumbnails/{video.title}.jpg', 'wb') as handler:
             handler.write(img_data)
         list_count += 1
         if list_count == 3:
             break
     return video_obj_list
+
+
+def get_tamplate_html():
+    with open("html_template.html", mode='r', encoding='utf-8') as template:
+        for line in template:
+            html_from_template = template.readlines()
+            #print(f'html lines are: {html_lines}')
+        return html_from_template
+
+
+def write_to_visual_list_html_file():
+    try:
+        with open(html_visual_list, mode='w', encoding='utf-8') as f:
+            for line in html_from_template[0:70]:
+                # print(line)
+                f.write(line)
+            for vid_obj in video_obj_list:
+                f.write(video_entry_html)
+            for line in html_from_template[70:74]:
+                f.write(line)
+    except Exception as e:
+        print(f"Something wrong with your file or name: \n{e}\n Lets try again")
+
+
+def create_playlist_data_backup():
+    try:
+        html_from_template = get_tamplate_html()
+        with open(html_visual_list, mode='w', encoding='utf-8') as f:
+            for line in html_from_template[0:70]:
+                # print(line)
+                f.write(line)
+            for vid_obj in video_obj_list:
+                f.write(video_entry_html)
+            for line in html_from_template[70:74]:
+                f.write(line)
+    except Exception as e:
+        print(f"Something wrong with your file or name: \n{e}\n Lets try again")
